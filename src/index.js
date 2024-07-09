a9bfb0167c98c18948d819aa685f7ddf2f4ef20e
Sisonke-dev <SIPHUZA@gmail.com>
2024-07-09 22:24:15 +0200

    added Axios

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ function search (Event) {
 
 let searchFormElement= document.querySelector("#search-form");
 let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchFormElement.value;
+  cityElement.textContent= searchFormElement.Value;
 }
