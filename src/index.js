7b5490101d3206212cf6380d33f8e1b0d67348ca
Sisonke-dev <SIPHUZA@gmail.com>
2024-07-09 23:07:25 +0200

    added more JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
+
+function searchCity(city){
+    let apiKey= "84t5f0f08be2e1a78e0fo88efb0b43e5";
+let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+}
 function search (Event) {
     Event.preventDefault();
 
 let searchFormElement= document.querySelector("#search-form");
 let cityElement = document.querySelector("#weather-app-city");
   cityElement.textContent= searchFormElement.Value;
+searchCity(searchInput.value);
 }
+axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
